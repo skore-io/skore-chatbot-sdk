3b1ead13c37102bb0e868b49fd4618bfa9fb782a
jluribe <jluribe@gmail.com>
2019-02-22 14:22:00 -0300

    add current user

--- skore/content_services.py
+++ skore/content_services.py
@@ -1,10 +1,15 @@
 from skore.base_services import BaseServices
 
 CHEF_API_URL = "{host}/chef/v1/chef/{recipe_id}?amountItemsByFlow={items}&amountFlows={flow}&grouped={grouped}"
+CURRENT_USER_URL = "{host}/workspace/v1/users/current"
 
 class ContentServices( BaseServices ):
   def __init__(self, host, app_token, user):
     super( ContentServices, self ).__init__(host, app_token, user)
+
+  def current_user(self):
+    url = CURRENT_USER_URL.format(host=self.host)    
+    return self.get(url).json()
 
   def recommendation(self, recipe_id, options = {'flow': 1, 'items':1, 'grouped': 'false'}):
     response = self.__query_chef(recipe_id, options)
