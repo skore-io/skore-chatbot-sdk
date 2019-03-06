780cb27983b0b5d2f2dc87490abd9264ae8171ce
jluribe <jluribe@gmail.com>
2019-03-05 21:03:11 -0300

    update auth service

--- skore/authentication_service.py
+++ skore/authentication_service.py
@@ -5,21 +5,19 @@ REFRESH_API_URL = "{host}/workspace/v1/refresh_token"
 CREATE_API_URL = "{host}/workspace/v1/users/{user_id}/sessions"
 
 class AuthenticationService:
-    def __init__(self, host, enviroment, company_id, skore_token):
+    def __init__(self, host, enviroment):
         self.enviroment = enviroment
         self.host = host
-        self.company_id = company_id
-        self.skore_token = skore_token
 
-    def login(self, email_or_username, password):
+    def login(self, email_or_username, password, company_id):
         data = {
-            'email': email_or_username, 
-            'password': password, 
-            'company_id': self.company_id
+            'email': email_or_username,
+            'password': password,
+            'company_id': company_id
         }
         headers = {
             'Content-Type': 'application/json'
-        }    
+        }
         response = requests.post(
             url=LOGIN_URL.format(url=self.host),
             json=data,
@@ -32,16 +30,16 @@ class AuthenticationService:
         url = REFRESH_API_URL.format(host=self.host)
         headers = { 'Content-Type': 'application/json' }
         data = {
-          'token': token, 
+          'token': token,
           'token_refresh': token_refresh
         }
         return requests.post(url=url, json=data, headers=headers)
 
-    def create_session(self, skore_user_id):
+    def create_session(self, skore_user_id, app_skore_token):
       url = CREATE_API_URL.format(host=self.host, user_id=skore_user_id)
       headers = {
           'Content-Type': 'application/json',
-          'Authorization': self.skore_token
+          'Authorization': app_skore_token
       }
       data = {
           'client_type': 'meya'
