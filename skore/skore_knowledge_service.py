95ec62c3df16ddb27e8c4c49fd1377ae7c6bf29b
jluribe <jluribe@gmail.com>
2019-03-11 16:36:59 -0300

    add get content provision

--- skore/skore_knowledge_service.py
+++ skore/skore_knowledge_service.py
@@ -2,10 +2,20 @@ import requests
 
 CURRENT_USER_URL = "{host}/workspace/v1/users/current"
 PREFERENCES_USER_URL = "{host}/workspace/v1/users/preferences"
+CONTENT_PROVISION_URL = "{host}/workspace/{version}/contents/{content_id}/provision"
 
 class SkoreKnowledgeService( object ):
   def __init__(self, host):
     self.host = host
+
+  def content_provision(self, token, content_id, version = 'v1'):
+    headers = {'Authorization': token }
+    url = CONTENT_PROVISION_URL.format(
+        host=self.host,
+        version=version,
+        content_id=content_id
+    )
+    return requests.get(url=url, headers=headers)
 
   def current_user(self, token):
     url = CURRENT_USER_URL.format(host=self.host)
